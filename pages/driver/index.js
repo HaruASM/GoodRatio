aa3a0feed98f3e80b63405fdd1a0769766ba3db5
JuneChoi <haruasm@gmail.com>
2025-01-19 13:37:51 +0900

    구글지도 권고사항 수정

--- pages/driver/index.js
+++ pages/driver/index.js
@@ -4,7 +4,9 @@ import Script from 'next/script';
 import { collection, getDocs } from 'firebase/firestore';
 import { firebasedb } from '../../firebase';
 
+
 const myAPIkeyforMAp = process.env.NEXT_PUBLIC_MAPS_API_KEY;
+
 
 export default function Driver() {
   const [status, setStatus] = useState('ready');
@@ -25,34 +27,42 @@ export default function Driver() {
 
     let map;
     let marker;
-
-    const loadMap = () => {
+    
+     const loadMap = () => {
       if (navigator.geolocation) {
         navigator.geolocation.watchPosition((position) => {
           const latitude = position.coords.latitude;
           const longitude = position.coords.longitude;
 
           const positionObj = { lat: latitude, lng: longitude };
-
+          
           if (!map) {
             map = new google.maps.Map(document.getElementById('map'), {
               center: positionObj,
               zoom: 15,
+              mapId: process.env.NEXT_PUBLIC_MAP_ID
             });
-
-            marker = new google.maps.Marker({
-              position: positionObj,
-              map,
-              title: '현재 위치',
-            });
+            loadMarker(positionObj);
           } else {
             map.setCenter(positionObj);
-            marker.setPosition(positionObj);
+            if (marker) {
+              marker.position = positionObj; // 위치 업데이트
+            }
           }
         });
       } else {
         alert("이 브라우저는 위치 정보를 지원하지 않습니다.");
       }
+    };
+
+    const loadMarker = async (positionObj) => {
+      const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+      marker = new AdvancedMarkerElement({
+        position: positionObj,
+        map,
+        title: '현재 위치',
+      });
+      return Promise.resolve(); // 마커 생성 후 Promise 반환
     };
 
     window.loadMap = loadMap;
@@ -119,8 +129,8 @@ export default function Driver() {
         </div>
         <div id="map" style={{ width: '100%', height: '500px' }}></div>
       </main>
-      <Script
-        src={`https://maps.googleapis.com/maps/api/js?key=${myAPIkeyforMAp}&callback=loadMap&v=beta`}
+      <Script 
+        src={`https://maps.googleapis.com/maps/api/js?key=${myAPIkeyforMAp}&callback=loadMap&v=beta&loading=async`}
         strategy="afterInteractive"
       />
     </div>
